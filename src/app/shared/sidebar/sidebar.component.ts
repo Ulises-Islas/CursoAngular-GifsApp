4445fa675a0e89df1764bd14a4cc7799164a4a60
Ulises-Islas <emyises18012@gmail.com>
2023-01-19 09:04:42 -0600

    1st commit

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  get historial() { return this.gifService.historial; }
+
+  constructor(private gifService: GifsService){ }
+
+  buscar(arg: string) {
+    this.gifService.buscarGifs(arg);
+  }
+
+}
